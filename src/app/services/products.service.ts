302232446a4879b74917b706e062615ec81573f0
Jonathan Merlino <jlm27921@gmail.com>
2019-12-01 03:20:39 -0300

    Fin

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -18,6 +18,7 @@ export class ProductsService {
   }
 
   private loadProducts() {
+    // tslint:disable-next-line: no-shadowed-variable
     return new Promise( (resolve, reject) => {
       this.http.get('https://angular-html-6c9f8.firebaseio.com/productos_idx.json')
         .subscribe(( resp: any[] ) => {
